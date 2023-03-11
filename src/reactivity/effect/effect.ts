0abe76055551d846b3cbe890ebf42a9960924484
aei <aei@aei.com>
2023-03-11 22:44:12 +0800

    feat: effect

---
+++ src/reactivity/effect/effect.ts
@@ -0,0 +1,88 @@
+/**
+ * effect 方法
+ *
+ * 思路：
+ * 	1.接收一个fn
+ * 	2.直接触发fn
+ *	3.提供收集依赖方法，当触发 get 方法时 调用 track 方法  进行依赖收集
+ *	4.提供执行依赖方法，当触发 set 方法时 调用 trigger 方法 执行收集到的所有依赖
+ *  5.提供stop方法，调用时停止传入runner的执行
+ */
+
+// 通过对象形式创建
+class ReactiveEffect {
+  private _fn: any;
+  deps = [];
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    return this._fn();
+  }
+  stop() {
+    this.deps.forEach((dep: any) => {
+      dep.delete(this);
+    });
+  }
+}
+
+let targetMaps = new Map();
+/**
+ *
+ * 收集依赖方法
+ *
+ */
+export function track(target, key) {
+  // target -> key -> dep
+  let depMaps = targetMaps.get(target);
+  // 处理初始化逻辑， 当初始化时没有 depMaps 就创建一个添加到 targetMaps 中
+  if (!depMaps) {
+    depMaps = new Map();
+    targetMaps.set(target, depMaps);
+  }
+  // 处理初始化逻辑， 当初始化时没有 dep 就创建 添加到 depMaps 中
+  let dep = depMaps.get(key);
+  if (!dep) {
+    dep = new Set();
+    depMaps.set(key, dep);
+  }
+  dep.add(activeEffect);
+  // 为了在执行 stop 方法时可以取到当前 effect 所有的 dep
+  activeEffect.deps.push(dep);
+}
+
+/**
+ *
+ * 依赖执行方法
+ *
+ */
+export function trigger(target, key) {
+  // 根据 target 获取到depMaps
+  let depMaps = targetMaps.get(target);
+  // 根据 key 获取到 deps
+  let deps = depMaps.get(key);
+  // 循环 deps 执行每一个 fn => run
+  for (const dep of deps) {
+    dep.run();
+  }
+}
+
+let activeEffect;
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+  const runner: any = _effect.run.bind(_effect);
+  // runner 挂载 effect 实例
+  runner.effect = _effect;
+  return runner;
+}
+
+/**
+ * stop
+ * 参数：fn
+ * 当调用此方法时，会停止 传入 fn 的响应式
+ */
+export function stop(runner) {
+  runner.effect.stop();
+}
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
-import { effect } from "../effect";
-import { reactive } from "../reactive";
+import { effect, stop } from "../effect/effect";
+import { reactive } from "../reactive/reactive";
 
 describe("effect", () => {
   it("happy path", () => {
@@ -13,5 +13,39 @@ describe("effect", () => {
     expect(nextAge).toBe(11);
     user.age++;
     expect(nextAge).toBe(12);
+  });
+  it("should return runner when call effect", () => {
+    // effect(fn) -> function (runner) -> fn() -> return
+    // 调用 effect  返回一个 runner 函数 效用runner  函数 返回用户传入fn 的返回值
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return "foo";
+    });
+    // 测试 effect 执行
+    expect(foo).toBe(11);
+    // 执行 effect 返回的函数
+    const r = runner();
+    expect(foo).toBe(12);
+    expect(r).toBe("foo");
+  });
+  // 测试 stop 功能
+  it("stop", () => {
+    let dummy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dummy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dummy).toBe(2);
+    // 执行 stop 停止 runner 响应式
+    stop(runner);
+    // obj.prop 改变时 不会更新
+    obj.prop = 3;
+    // 所以结果 dummy 还是 2
+    expect(dummy).toBe(2);
+    // 重新执行 runner runner 重新打开响应式  可以更寻 dummy
+    runner();
+    expect(dummy).toBe(3);
   });
 });
