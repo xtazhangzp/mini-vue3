185ae55b5e9e2c6b5fb6e7cc6c8ca402344a0e3d
aei <aei@aei.com>
2023-04-15 23:13:02 +0800

    feat: propx

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -1,12 +1,18 @@
+import { hasOwn } from "../reactivity/shared";
+
 const publicPropertiesMap = {
   $el: (i) => i.vnode.el,
 };
 
 export const PublicInstanceProxyHandlers = {
   get({ _: instance }, key) {
-    const { setupState } = instance;
-    if (key in setupState) {
+    const { setupState, props } = instance;
+    if (hasOwn(setupState, key)) {
       return setupState[key];
+    }
+    // 处理 组件传入的 props
+    if (hasOwn(props, key)) {
+      return props[key];
     }
     // 判断key 是不是$el 如果是 就返回 vnode 的el
     const publicGetter = publicPropertiesMap[key];
